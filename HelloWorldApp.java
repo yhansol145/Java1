4da4249ebf7be1361a74ed52a0f3f804c40b519b
HANSOL <yhansol145@gmail.com>
2021-03-03 12:49:36 +0900

    Add introduce Name

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,5 +1,6 @@
 public class HelloWorldApp {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!!");
+		System.out.println("Hello World!!");
+		System.out.println("My name is Hansol YOO!!");
 	}
 }
