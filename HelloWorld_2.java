531b00afc79bf2a686d34365826ff7d9df826acc
HANSOL <yhansol145@gmail.com>
2021-03-04 18:38:20 +0900

    This is the second test !

---
+++ HelloWorld_2.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World_2");
+		System.out.println("This is the second test!");
+
+	}
+
+}
